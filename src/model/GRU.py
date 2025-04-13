98188172c51f104a53ddeaa4a5c35b410763fefe
noorxman <noor1mansour@outlook.com>
2025-04-13 21:33:37 +0200

    Second commit

---
+++ src/model/GRU.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class GRUForecaster(nn.Module):
+    def __init__(self, input_size=1, hidden_size=64, num_layers=2, output_size=1):
+        super(GRUForecaster, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        out, _ = self.gru(x, h0)
+        out = self.fc(out[:, -1, :])
+        return out
